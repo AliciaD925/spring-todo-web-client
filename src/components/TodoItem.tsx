0e50d709fe432c85e087b6c216c9f70dcc720545
Alicia Diller <aliciad925@gmail.com>
2020-06-02 08:38:24 -0700

    Add Modal

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Button, Card, Checkbox, Col} from 'antd'
+import {Button, Card, Checkbox, Col, Modal} from 'antd'
 import Todo from "../services/Todo";
 import styles from './styles.module.css';
 import {deleteTodoById} from "../services/todoService";
@@ -8,15 +8,39 @@ interface TodoProp {
    todo: Todo;
    reload(): void;
 }
+interface TodoState {
+    modalVisible: boolean;
+}
 
-class TodoItem extends Component<TodoProp>{
+class TodoItem extends Component<TodoProp, TodoState>{
+    private handleOk: (e: any) => void;
+    private handleCancel: (e: any) => void;
+    private showModal: () => void;
     constructor(props: TodoProp){
         super(props);
+        this.state = {
+            modalVisible: false
+        }
 
+        this.showModal = () => {
+            this.setState ({modalVisible: true, });
+        };
+
+        this.handleOk =(e: any) => {
+            console.log(e);
+            this.setState ({modalVisible: false, });
+        };
+
+        this.handleCancel = (e: any) => {
+            console.log(e);
+            this.setState ({modalVisible: false, });
+        }
     }
     deleteTodo = () => {
-        if(this.props.todo.id != null) {
+        if(this.props.todo.id != null && this.props.todo.complete) {
             deleteTodoById(this.props.todo.id).then(() => this.props.reload());
+        } else {
+            this.showModal();
         }
     }
     render() {
@@ -26,9 +50,17 @@ class TodoItem extends Component<TodoProp>{
                     <p>Description:</p>
                     <p>{this.props.todo.description}</p>
                     <p>Complete: <Checkbox defaultChecked={this.props.todo.complete} disabled /></p>
-                 <Button type= "primary" danger onClick={this.deleteTodo}>
+
+                    <Button type= "primary" danger onClick={this.deleteTodo}>
                      Delete
                  </Button>
+                    <Modal
+                    title="Delete Todo"
+                    visible={this.state.modalVisible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}>
+                        <p className={styles.dangerDeleteTodo}> In order to delete a todo, it MUST be completed</p>
+                    </Modal>
                 </Card>
             </Col>
         );
